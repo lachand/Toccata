bf9a000ed001976dce2fbf1333ca57a7c46e4aaf
lachand <valentin@lachand.net>
2018-03-23 15:49:57 +0100

    Update of installation note (work in progress)

--- src/variables.ts
+++ src/variables.ts
@@ -2,19 +2,19 @@
  * Host of the couchdb server
  * @type {string} Host of the couchdb server
  */
-export const HOST = '';
+export const HOST = process.env.HOST;
 /**
  * Port of the couchdb server
  * @type {string} Port of the couchdb server
  */
-export const PORT = '';
+export const PORT = process.env.PORT;
 /**
  * Username of the couchdb server
  * @type {string} Username of the couchdb server
  */
-export const USERNAME = '';
+export const USERNAME = process.env.USERNAME;
 /**
  * Password of the couchdb server
  * @type {string} Password of the couchdb server
  */
-export const PASSWORD = '';
+export const PASSWORD = process.env.PASSWPRD;
