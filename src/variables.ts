73ca87329fff297f916c7c14070d5db968942da0
lachand <valentin@lachand.net>
2018-03-29 11:12:44 +0200

    test debug

--- src/variables.ts
+++ src/variables.ts
@@ -2,19 +2,19 @@
  * Host of the couchdb server
  * @type {string} Host of the couchdb server
  */
-export const HOST = '';
+export const HOST = '37.59.63.190';
 /**
  * Port of the couchdb server
  * @type {string} Port of the couchdb server
  */
-export const PORT = ':';
+export const PORT = ':8080';
 /**
  * Username of the couchdb server
  * @type {string} Username of the couchdb server
  */
-export const USERNAME = '';
+export const USERNAME = 'root';
 /**
  * Password of the couchdb server
  * @type {string} Password of the couchdb server
  */
-export const PASSWORD = '';
+export const PASSWORD = 'mdproot';
