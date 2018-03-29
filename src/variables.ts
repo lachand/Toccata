d784e4e76605499ce49e6febd55ab32d5de34a8c
lachand <valentin@lachand.net>
2018-03-29 08:56:23 +0200

    debug

--- src/variables.ts
+++ src/variables.ts
@@ -2,19 +2,19 @@
  * Host of the couchdb server
  * @type {string} Host of the couchdb server
  */
-export const HOST = process.env.HOST;
+export const HOST = '';
 /**
  * Port of the couchdb server
  * @type {string} Port of the couchdb server
  */
-export const PORT = process.env.PORT;
+export const PORT = ':';
 /**
  * Username of the couchdb server
  * @type {string} Username of the couchdb server
  */
-export const USERNAME = process.env.USERNAME;
+export const USERNAME = '';
 /**
  * Password of the couchdb server
  * @type {string} Password of the couchdb server
  */
-export const PASSWORD = process.env.PASSWPRD;
+export const PASSWORD = '';
