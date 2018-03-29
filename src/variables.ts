4a0356102b3d7b3380cb60c613c9d564316c2ca0
lachand <valentin@lachand.net>
2018-03-29 16:52:10 +0200

    Debug

--- src/variables.ts
+++ src/variables.ts
@@ -2,7 +2,7 @@
  * Host of the couchdb server
  * @type {string} Host of the couchdb server
  */
-export const HOST = '37.59.63.190';
+export const HOST = 'http://37.59.63.190';
 /**
  * Port of the couchdb server
  * @type {string} Port of the couchdb server
