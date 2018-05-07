4d426a04be6d248e054f18d7353727dbefed2808
lachand <valentin@lachand.net>
2018-05-07 16:04:17 +0200

    test for heroku

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  production: true,
+  URL_DB: "http://37.59.63.190",
+  PORT_DB: ":8080",
+  USERNAME_DB: "root",
+  PASSWORD_DB: "mdproot",
+  ROOM: "pc_valentin"
+};
