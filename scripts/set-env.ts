8ccc21ad8d9d6f5bcb7b80ecbd569af14f9e9cc4
lachand <valentin@lachand.net>
2018-05-09 11:01:23 +0200

    New version for heroku

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -8,6 +8,7 @@ require('dotenv').config();
 // Would be passed to script like this:
 // `ts-node set-env.ts --environment=dev`
 // we get it from yargs's argv object
+
 const environment = argv.environment;
 const isProd = environment === 'prod';
 
