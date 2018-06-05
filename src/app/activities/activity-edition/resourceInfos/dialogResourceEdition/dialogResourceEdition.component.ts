82235c5fc203d25f6095bca908d1d8d9cb54e55b
lachand <valentin@lachand.net>
2018-06-05 11:10:00 +0200

    Add resource edition

---
+++ src/app/activities/activity-edition/resourceInfos/dialogResourceEdition/dialogResourceEdition.component.ts
@@ -0,0 +1,50 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'dialog-resource-edition',
+  templateUrl: './dialogResourceEdition.component.html',
+})
+
+/**
+ * Create a dialog to ask user for group name
+ */
+export class DialogResourceEditionComponent implements OnInit{
+  message: String;
+  resourceNameForm: FormGroup;
+  errorResourceName: any;
+
+  /**
+   * Create the dialog
+   * @param data
+   */
+  constructor(public formBuilder: FormBuilder, private dialogRef: MatDialogRef<DialogResourceEditionComponent>) {
+  }
+
+  ngOnInit() {
+    this.errorResourceName = false;
+    this.resourceNameForm = this.formBuilder.group({
+      resourcename: ''
+    });
+  }
+
+  /**
+   * Close the dialog and create a duplicate
+   */
+  validate() {
+    if (this.resourceNameForm.value.resourcename === '') {
+      this.errorResourceName = true;
+    } else {
+      this.dialogRef.close({type: 'validate', value: this.resourceNameForm.value.resourcename});
+    }
+  }
+
+  /**
+   * Close the dialog
+   */
+  close_dialog() {
+    this.dialogRef.close({type: 'close'});
+  }
+
+}
