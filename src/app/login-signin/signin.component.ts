501371b7311b9683c99951e075254f21f6058a51
lachand <valentin@lachand.net>
2018-06-08 10:01:55 +0200

    Add card for inscription

--- src/app/login-signin/signin.component.ts
+++ src/app/login-signin/signin.component.ts
@@ -12,12 +12,14 @@ import {Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-signin',
-  templateUrl: './signin.component.html'
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
 
   signinForm: FormGroup;
   teacher: any;
+  error = Array<any>;
 
   constructor(public user: UserService,
               public router: Router,
@@ -34,10 +36,50 @@ export class SigninComponent implements OnInit {
       avatar: 'http://mondesfrancophones.com/wp-content/Cimy_User_Extra_Fields/clarouche/Christian%20Larouche.JPG'
     });
     this.teacher = false;
+    this.errorReset();
+  }
+
+  errorReset() {
+    this.error['passwordCheck'] = false;
+    this.error['passwordEmpty'] = false;
+    this.error['nameEmpty'] = false;
+    this.error['usernameEmpty'] = false;
+    this.error['surnameEmpty'] = false;
+    this.error['avatarEmpty'] = false;
+  }
+
+  checked() {
+    let checked = true;
+    if (this.signinForm.value.password !== this.signinForm.value.passwordCheck){
+      this.error['passwordCheck'] = true;
+      checked = false;
+    }
+    if (this.signinForm.value.password === '') {
+      this.error['passwordEmpty'] = true;
+      checked = false;
+    }
+    if (this.signinForm.value.name === '') {
+      this.error['nameEmpty'] = true;
+      checked = false;
+    }
+    if (this.signinForm.value.username === '') {
+      this.error['usernameEmpty'] = true;
+      checked = false;
+    }
+    if (this.signinForm.value.surname === '') {
+      this.error['surnameEmpty'] = true;
+      checked = false;
+    }
+    if (this.signinForm.value.avatar === '') {
+      this.error['avatarEmpty'] = true;
+      checked = false;
+    }
+    return checked;
   }
 
   signup(): void {
-    if (this.signinForm.valid) {
+    this.errorReset();
+    if (this.checked()) {
 
       const headers = new Headers();
       headers.append('Content-Type', 'application/json');
