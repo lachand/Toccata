e876373c545eff6a2bad9c442762d959206d61d5
valentin <valentin@lachand.net>
2018-09-14 11:58:38 +0200

    Add travis support

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  DB: '',
+  URL_DB: '',
+  USERNAME_DB: '',
+  PASSWORD_DB: '',
+  USERNAME_DB_BACKUP: '',
+  PASSWORD_DB_BACKUP: '',
+  ROOM: '',
+  URL_PORT: ''
+};
