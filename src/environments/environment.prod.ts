41de7f91bd85310b72e4262c0e34b87f02dcaa3d
valentin <valentin@lachand.net>
2018-09-17 11:53:22 +0200

    Add travis support for karma

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
+
 export const environment = {
   production: true,
-  DB: process.env.DB,
-  URL_DB: process.env.URL_DB,
-  USERNAME_DB: process.env.USERNAME_DB,
-  PASSWORD_DB: process.env.PASSWORD_DB,
-  ROOM: process.env.ROOM,
-  URL_PORT: process.env.URL_PORT,
-  envName: 'prod'
+  DB: "toccata_empty",
+  URL_DB: "https://8138ef3c-4d19-4661-ac34-29c2f37fe002-bluemix.cloudant.com",
+  USERNAME_DB: "weenedoonleproughtytoged",
+  PASSWORD_DB: "252db8fca3a7ae861d54e9458b5c1eb96fe5acdb",
+  ROOM: "Externe",
+  URL_PORT: "443",
+  envName: "prod"
 };
