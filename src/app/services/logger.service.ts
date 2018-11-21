8b9ba49529c1db7b3f21205ff26c073db5e77d66
valentin <valentin@lachand.net>
2018-11-21 09:25:39 +0100

    Add functions documentation

--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -10,10 +10,18 @@ export class LoggerService {
   logDocument: any;
   doc: any;
 
+  /**
+   * Construct the logger
+   * @param user User service to obtain user connected
+   * @param database Database service used to update log file
+   */
   constructor(private user: UserService, private database: DatabaseService) {
     this.logFile = window.localStorage;
   }
 
+  /**
+   * Create an empty log file for the session
+   */
   initLog() {
     this.logName = Date.now();
     this.logDocument = `Log_${this.user.id}_${this.logName}`;
@@ -33,6 +41,14 @@ export class LoggerService {
     this.cpt++;
   }
 
+  /**
+   * Update the log file
+   * @param actionType Type of action to write
+   * @param activity Current activity
+   * @param object Object of the action
+   * @param message Message for the action
+   * @param system Value to know if the action is initiated by the system or the user
+   */
   log(actionType, activity, object, message, system: boolean = false) {
     let initiatedBy = 'User';
     if (system) {
